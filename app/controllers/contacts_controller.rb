85db190716bc73c8836d96c1d4a3bd68d8191ea8
Sander Sijbrandij <ssijbrandij@gmail.com>
2016-11-25 19:15:36 +0100

    added comments

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,18 +1,25 @@
 class ContactsController < ApplicationController
+
+  # GET /contact-us
   def new
     @contact = Contact.new
   end
 
+  # POST /contact-us
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
+      # Send email
       name = params[:contact][:name]
       email = params[:contact][:email]
       body = params[:contact][:comments]
       ContactMailer.contact_email(name, email, body).deliver
+
+      # Redirect with flash
       flash[:success] = "Message sent."
       redirect_to new_contact_path
     else
+      # Redirect with flash containing errors
       flash[:danger] = @contact.errors.full_messages.join(", ")
       redirect_to new_contact_path
     end
