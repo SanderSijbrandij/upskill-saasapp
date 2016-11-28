13f301e17a9aef5fa5130c04057b6fd20f4fa095
Sander Sijbrandij <ssijbrandij@gmail.com>
2016-11-28 21:12:10 +0100

    added profile create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,26 @@ class ProfilesController < ApplicationController
 
   # GET /users/:user_id/profile/new
   def new
-    @profile = Profile.new(user_id: params[:user_id])
+    @profile = Profile.new
   end
+
+  # POST /users/:user_id/profile
+  def create
+    @user = User.find(params[:user_id])
+    @profile = @user.build_profile(profile_params)
+
+    if @profile.save
+      flash[:success] = "Profile updated!"
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+
+  private
+    def profile_params
+      params.require(:profile).permit(:first_name, :last_name, :job_title,
+                                      :phone_number, :contact_email,
+                                      :description)
+    end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,10 @@
 class Profile < ApplicationRecord
   belongs_to :user
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :job_title, presence: true
+  validates :phone_number, presence: true
+  validates :contact_email, presence: true
+  validates :description, presence: true
 end
