6aa7188fb756020d8d88eeba02f72929d4871177
Sander Sijbrandij <ssijbrandij@gmail.com>
2016-11-29 20:24:09 +0100

    Add profile edit/update actions

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -11,10 +11,27 @@ class ProfilesController < ApplicationController
     @profile = @user.build_profile(profile_params)
 
     if @profile.save
-      flash[:success] = "Profile updated!"
+      flash[:success] = "Profile created!"
       redirect_to user_path(@user)
     else
       render action: :new
+    end
+  end
+
+  # GET /users/:user_id/profile/edit
+  def edit
+    @profile = User.find(params[:user_id]).profile
+  end
+
+  # PUT   /users/:user_id/profile
+  # PATCH /users/:user_id/profile
+  def update
+    @profile = User.find(params[:user_id]).profile
+    if @profile.update_attributes(profile_params)
+      flash[:success] = "Profile updated!"
+      redirect_to user_path(@profile.user)
+    else
+      render action: :edit
     end
   end
 
